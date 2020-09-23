271d522488a7ddfd3b496f53f1da6449dccc530e
Crush302 <mazich.joe@gmail.com>
2020-09-22 19:31:12 -0500

    Made really hacky ways for connection detection and parsing HTTP request

--- http_server1.py
+++ http_server1.py
@@ -6,19 +6,41 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('localhost', 1900))
 s.listen(5)
 
+def sendHeader(code, length, type):
+    # rewrite into switch
+    if code == 200:
+        code = '200 OK'
+    elif code == 400:
+        code = '400 Bad Request'
+    elif code == 403:
+        code = '403 Forbidden'
+    elif code == 404:
+        code = '404 Not Found'
+    return ('HTTP/1.1 %s \r\nContent-Length: %s\r\nContent-Tpye: \r\n\r\n' %(code, length, type))
+
 while True:
     clientSock, clientAddr = s.accept()
     fullResponse = clientSock.recv(4096).decode()
-    print(fullResponse)
 
-    # Same header parsing as the curl clone
-    header = {}
-    for line in fullResponse.split('\n\r\n')[0].split('\n'):
-        if ':' in line:
-            x, y = line.split(':', 1)
-            header[x] = y.strip()
+    # This is such a hacky way to do this, really want to change this (working as a Connection detector)
+    if len(fullResponse) > 0:
+        # Same header parsing as the curl clone
+        header = {}
+        firstLine = fullResponse.split('\n')[0].split(' ')
+        header['HTTP-Command'] = firstLine[0]
+        header['Path'] = firstLine[1]
+        header['HTTP-Type'] = firstLine[2]
+        for line in fullResponse.split('\n\r\n')[0].split('\n'):
+            if ':' in line:
+                x, y = line.split(':', 1)
+                header[x] = y.strip()
 
-    # Same loop to make sure we get all the data
-    while len(fullResponse) < int(header['Content-Length']):
-        response = client.recv(4096)
-        fullResponse += response.decode()
+        '''# Same loop to make sure we get all the data
+        while len(fullResponse) < int(header['Content-Length']):
+            response = client.recv(4096)
+            fullResponse += response.decode()'''
+
+        if header['HTTP-Command'] != 'GET':
+            clientSock.send(sendHeader(400, 0, 'text/html').encode())
+        else:
+            print(header['Path'])
